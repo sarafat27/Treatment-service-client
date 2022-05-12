5f19807d5b97ce918b71f448946498081807ee9f
sarafat27 <sarafat2769@gmail.com>
2022-05-12 17:20:45 +0600

    modal created

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
     const { name, slots } = service
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
@@ -15,7 +15,10 @@ const Service = ({ service }) => {
                 </p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
                 <div className="card-actions justify-center">
-                    <button disabled={slots.length === 0} className="btn btn-secondary text-white uppercase">Book appointment</button>
+                    <label for="booking-modal"
+                        onClick={() => setTreatment(service)}
+                        disabled={slots.length === 0}
+                        className="btn btn-secondary text-white uppercase">Book appointment</label>
                 </div>
             </div>
         </div>
